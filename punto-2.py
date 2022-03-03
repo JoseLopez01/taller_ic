df11750443891cca46720fa02c2ee3c9ea686a03
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-03-02 19:34:02 -0500

    Fix: punto 2 simbolo

--- punto-2.py
+++ punto-2.py
@@ -4,10 +4,10 @@ third_capital = float(input('Inversion de la tercera persona \n'))
 
 total = first_capital + second_capital + third_capital
 
-first_percentage = first_capital / total
-second_percentage = second_capital / total
-third_percentage = third_capital / total
+first_percentage = (first_capital / total) * 100
+second_percentage = (second_capital / total) * 100
+third_percentage = (third_capital / total) * 100
 
-print(f'Porcentage primer inversionista {first_percentage}')
-print(f'Porcentage segundo inversionista {second_percentage}')
-print(f'Porcentage tercer inversionista {third_percentage}')
+print(f'Porcentage primer inversionista {first_percentage}%')
+print(f'Porcentage segundo inversionista {second_percentage}%')
+print(f'Porcentage tercer inversionista {third_percentage}%')
